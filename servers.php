831aea329e837a7bc5d7af9ba2d1c91a3273bb5d
wwreyti <147952458+wwreyti@users.noreply.github.com>
2025-02-04 17:20:10 +0300

    Update servers.php

--- servers.php
+++ servers.php
@@ -8,6 +8,9 @@ $aServerRules = $query->getRules();
 
 [
     {
+      "ip": "54.36.82.238",
+      "port": 1373,
+      "x2": false,
       "color": "00FF00",
       "dopname": "",
       "maxonline": <?= $aInformation['maxplayers'] ?>,

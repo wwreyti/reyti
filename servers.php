1a3f9f407ea44178fac496e349b8e9e81c81d813
wwreyti <147952458+wwreyti@users.noreply.github.com>
2025-02-04 17:06:59 +0300

    1.5 servers.php

---
+++ servers.php
@@ -0,0 +1,17 @@
+<?
+include ('SampQueryAPI.php');  // Сам инклуд, и инфа внутри
+$query = new SampQueryAPI('185.189.255.97', '1293'); // Ип, и порт для соединения
+$aInformation = $query->getInfo(); // Переменная как в PAWN, чуть отличается от PAWN
+$aServerRules = $query->getRules(); 
+?>
+
+
+[
+    {
+      "color": "00FF00",
+      "dopname": "",
+      "maxonline": <?= $aInformation['maxplayers'] ?>,
+      "name": "Green",
+      "online": <?= $aInformation['players'] ?>
+    }
+]

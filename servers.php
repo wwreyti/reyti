b20ba496de20db964df225b5e7f279794167e2e7
wwreyti <147952458+wwreyti@users.noreply.github.com>
2025-02-04 17:32:40 +0300

    1.1 servers.php

--- servers.php
+++ servers.php
@@ -1,20 +1,22 @@
-<?
-include ('SampQueryAPI.php');  // Сам инклуд, и инфа внутри
-$query = new SampQueryAPI('185.189.255.97', '1293'); // Ип, и порт для соединения
-$aInformation = $query->getInfo(); // Переменная как в PAWN, чуть отличается от PAWN
-$aServerRules = $query->getRules(); 
+<?php
+include ('SampQueryAPI.php');
+$query = new SampQueryAPI('185.189.255.97', '1293');
+$aInformation = $query->getInfo();
+$aServerRules = $query->getRules();
+
+$response = [
+    [
+        "ip" => "54.36.82.238",
+        "port" => 1373,
+        "x2" => false,
+        "color" => "00FF00",
+        "dopname" => "",
+        "maxonline" => $aInformation['maxplayers'],
+        "name" => "Green",
+        "online" => $aInformation['players']
+    ]
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
-
-
-[
-    {
-      "ip": "54.36.82.238",
-      "port": 1373,
-      "x2": false,
-      "color": "00FF00",
-      "dopname": "",
-      "maxonline": <?= $aInformation['maxplayers'] ?>,
-      "name": "Green",
-      "online": <?= $aInformation['players'] ?>
-    }
-]

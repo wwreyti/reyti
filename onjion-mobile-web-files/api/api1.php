c424cfdf01ed19d573eef3d5fb42ee193213334f
wwreyti <147952458+wwreyti@users.noreply.github.com>
2025-03-02 23:52:19 +0300

    Update api1.php

--- onjion-mobile-web-files/api/api1.php
+++ onjion-mobile-web-files/api/api1.php
@@ -1,91 +1,137 @@
-<?php
-header('Content-Type: application/json'); // Устанавливаем заголовок для вывода JSON
-
-$data = [
-    "launcher_version" => 33,
-    "launcher_url" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/launcher_space.apk?edgar=1829top09",
-    "launcher_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/launcher_onjion.apk",
-    "launcher_name" => "/launcher_onjion.apk",
-    "isTest" => false,
-    "test_api" => false,
-    "api" => "https://wwreyti.github.io/reyti/onjion-mobile-web-files/api/main.json?edgar=1829top09",
-    "archives" => [
-        [
-            "type" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
-            "zip_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_txd1.zip",
-            "size" => 545036062,
-            "urls" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_txd1.zip?edgar=1829top09",
-            "paths" => [
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta_int"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta3"]
-            ]
-        ],
-        [
-            "type" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
-            "zip_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_txd2.zip",
-            "size" => 185915466,
-            "urls" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_txd2.zip?edgar=1829top09",
-            "paths" => [
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gui"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/menu"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/mobile"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/player"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/radar"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/samp"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/txd"]
-            ]
-        ],
-        [
-            "type" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
-            "zip_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_img1.zip",
-            "size" => 356608000,
-            "urls" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_img1.zip?edgar=1829top09",
-            "paths" => [
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta_int.img"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/player.img"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/samp.img"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/SAMPCOL.img"]
-            ]
-        ],
-        [
-            "type" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
-            "zip_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_img2.zip",
-            "size" => 687067136,
-            "urls" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_img2.zip?edgar=1829top09",
-            "paths" => [
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta3.img"]
-            ]
-        ],
-        [
-            "type" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files",
-            "zip_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/archive_lite.zip",
-            "size" => 111827652,
-            "urls" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_lite.zip?edgar=1829top09",
-            "paths" => [
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/data"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/models"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/ONJION"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/anim"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/audio"]
-            ]
-        ],
-        [
-            "type" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files",
-            "zip_path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/archive_config.zip",
-            "size" => 131403,
-            "urls" => "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_config.zip?edgar=1829top09",
-            "paths" => [
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/Adjustable.cfg"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/spa_ce.set"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/SPACE10.b"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/spacetelem.set"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/stream.ini"],
-                ["path" => "/storage/emulated/0/Android/data/ru.onjion.mobile/files/version.txt"]
-            ]
-        ]
+{
+	"launcher_version": 33,
+	"launcher_url": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/launcher_space.apk?edgar=1829top09",
+	"launcher_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/launcher_onjion.apk",
+	"launcher_name": "/launcher_onjion.apk",
+	"isTest": false,
+	"test_api": false,
+    "api": "https://wwreyti.github.io/reyti/onjion-mobile-web-files/api/main.json?edgar=1829top09",
+	"archives": [
+        {
+            "type": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_txd1.zip",
+            "size": 545036062,
+            "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_txd1.zip?edgar=1829top09",
+			"paths": [
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta_int"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta3"
+				}
+			]
+        },
+		{
+            "type": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_txd2.zip",
+            "size": 185915466,
+            "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_txd2.zip?edgar=1829top09",
+			"paths": [
+				
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gui"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/menu"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/mobile"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/player"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/radar"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/samp"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/txd"
+				}
+			]
+        },
+		{
+            "type": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_img1.zip",
+            "size": 356608000,
+            "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_img1.zip?edgar=1829top09",
+			"paths": [
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta_int.img"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/player.img"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/samp.img"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/SAMPCOL.img"
+				}
+			]
+        },
+		{
+            "type": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/archive_img2.zip",
+            "size": 687067136,
+            "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_img2.zip?edgar=1829top09",
+			"paths": [
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/texdb/gta3.img"
+				}
+			]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.onjion.mobile/files",
+			"zip_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/archive_lite.zip",
+            "size": 111827652,
+            "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_lite.zip?edgar=1829top09",
+			"paths": [
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/data"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/models"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/ONJION"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/anim"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/audio"
+				}
+			]
+        },
+        {
+            "type": "/storage/emulated/0/Android/data/ru.onjion.mobile/files",
+			"zip_path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/archive_config.zip",
+            "size": 131403,
+            "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_config.zip?edgar=1829top09",
+			"paths": [
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/Adjustable.cfg"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/spa_ce.set"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/SPACE10.b"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/spacetelem.set"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/stream.ini"
+				},
+				{
+					"path": "/storage/emulated/0/Android/data/ru.onjion.mobile/files/version.txt"
+				}
+			]
+        }
     ],
-    "deleted" => []
-];
-
-echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES); // Выводим данные в формате JSON
-?>
+	"deleted": [
+	]
+}
